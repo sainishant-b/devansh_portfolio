ccbbe72872ecf18500a6b4dbce1d13138b2db298
devansh7439 <devanshbagaria2007@gmail.com>
2026-02-05 15:05:51 +0530

    Update page.tsx

--- v1/app/page.tsx
+++ v1/app/page.tsx
@@ -1,35 +1,23 @@
 "use client";
 
 import { useRef } from "react";
-import { useScroll } from "framer-motion";
 import { ScrollyCanvas } from "@/components/custom/ScrollyCanvas";
-import { Overlay } from "@/components/custom/Overlay";
 import Projects  from "@/components/Projects";
 import Skills from "@/components/Skills";
 import ContactMe from "@/components/ContactMe";
 import AboutMe from "@/components/Aboutme";
 
 export default function Home() {
-  const containerRef = useRef<HTMLDivElement>(null);
-  
-  const { scrollYProgress } = useScroll({
-    target: containerRef,
-    offset: ["start start", "end end"],
-  });
-
   return (
     <main className="relative bg-[#121212]">
       {/* Scrollytelling Section */}
-      <section ref={containerRef} className="relative">
+      <section className="relative">
         {/* Canvas-based frame sequence */}
         <ScrollyCanvas 
           frameCount={147} 
           framePath="/sequence/webp"
           scrollHeight="500vh"
         />
-        
-        {/* Overlay with parallax text */}
-        <Overlay scrollYProgress={scrollYProgress} />
       </section>
       <AboutMe/>
 
